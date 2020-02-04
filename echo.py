2012671ea07452eb49cf9d4c647c5d5f8a4b301f
Jeremy Kenyon <lanthos@gmail.com>
2020-02-04 11:39:23 -1000

    DEV-1596 base server created for testing flow.  Will need to still create endpoints for doing all of the special echo stuff.

--- echo.py
+++ echo.py
@@ -1,4 +1,15 @@
+import datetime
 from flask import Flask
+from flask import request
+from flask import url_for
+from transactid.transactid import TransactID
+from transactid.exceptions import InvalidSignatureException
+from transactid.exceptions import DecodeException
+
+from dummy_keys import DUMMY_PRIVATE_KEY
+from dummy_keys import DUMMY_CERT
+
+transact = TransactID(private_key_pem=DUMMY_PRIVATE_KEY, certificate_pem=DUMMY_CERT)
 app = Flask(__name__)
 
 
@@ -7,6 +18,97 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route("/invoice-request")
+@app.route("/initial-invoice-request")
+def initial_invoice_request():
+    """
+    * amount: Optional[int]  # amount is integer-number-of-satoshis
+    * pki_type: Optional[str]  # Currently only x509+sha256 and the string none are supported.
+    * memo: Optional[str]
+    * notification_url: Optional[str]
+    """
+    serialized_invoice_request = transact.create_invoice_request(
+        pki_type="x509+sha256",
+        memo="This is a demo request.",
+        notification_url=url_for("payment_request", _external=True)
+    )
+
+    return serialized_invoice_request
+
+
+@app.route("/invoice-request", methods=["POST"])
 def invoice_request():
-    pass
+    """
+    * time_stamp: datetime
+    * outputs: List[(int, bytes)]  # int: amount, bytes: script (see BIP70 details for more information on scripts)
+    * memo: str
+    * payment_url: str
+    * merchant_data: bytes
+    * expires: Optional[datetime]
+    * pki_type: Optional[str]
+    * network: str, defaults to "main"
+    * payment_details_version: int, defaults to 1
+    """
+    try:
+        transact.verify_invoice_request(request.data)
+    except InvalidSignatureException:
+        return "Invalid Signature received"
+    except DecodeException:
+        return "Unable to decode protobuf object"
+    else:
+        invoice_request = transact.get_verified_invoice_request()
+        print(invoice_request)
+        # here is where you would normally do business logic against the data you received
+        serialized_payment_request = transact.create_payment_request(
+            time_stamp=datetime.datetime.now(),
+            outputs=[(2052020, b"stan LOONA")],  # you would normally put a regular blockchain script here
+            memo=invoice_request["memo"],
+            payment_url=url_for("payment", _external=True),
+            merchant_data=b"merchant identifier goes here (optional)",
+            pki_type="x509+sha256"
+        )
+
+        return serialized_payment_request
+
+
+@app.route("/payment-request", methods=["POST"])
+def payment_request():
+    """
+    * transactions: List[bytes]
+    * refund_to: List[(int, bytes)]  # int: amount, bytes: script (see BIP70 details for more information on scripts)
+    * merchant_data: Optional[bytes]
+    * memo: Optional[str]
+    """
+    try:
+        transact.verify_payment_request(request.data)
+    except InvalidSignatureException:
+        return "Invalid Signature received"
+    except DecodeException:
+        return "Unable to decode protobuf object"
+    else:
+        payment_request = transact.get_verified_payment_request()
+        print(payment_request)
+        # here is where you would normally do business logic against the data you received
+
+        serialized_payment = transact.create_payment(
+            transactions=[b"blockchain transactions go here"],
+            refund_to=[(2052020, b"stan LOONA")],  # you would normally put a regular blockchain script here
+            memo="here's your payment",
+            merchant_data=b"merchant identifier goes here (optional)",
+        )
+        return serialized_payment
+
+
+@app.route("/payment", methods=["POST"])
+def payment():
+
+    try:
+        transact.verify_payment(request.data)
+    except DecodeException:
+        return "Unable to decode protobuf object"
+    else:
+        payment = transact.get_verified_payment()
+        print(payment)
+        # here is where you would normally do business logic against the data you received
+        serialized_payment_ack = transact.create_payment_ack(memo="thanks for the coin chummer")
+
+        return serialized_payment_ack
